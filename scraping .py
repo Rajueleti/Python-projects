c350d46001d51ba98a75efb94e0482e2a4cbb338
Rajueleti <yalati.raju25@gmail.com>
2022-10-06 17:04:08 +0530

    url change

--- scraping .py
+++ scraping .py
@@ -3,7 +3,7 @@ from pydoc import classify_class_attrs
 from urllib import request
 import requests
 from bs4 import BeautifulSoup
-req= requests.get("https://www.truecaller.com/search/in/9866486658")
+req= requests.get("https://www.truecaller.com")
 soup= BeautifulSoup(req.content, "html.parser")
 #print(soup.prettify)
 s=s
